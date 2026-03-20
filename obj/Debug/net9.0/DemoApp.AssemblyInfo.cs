a641cf3689842b67661c38b0fe9bd3c8fa248e82
Sravanthi-88 <sravanthikumarapu.88@gmail.com>
2026-03-20 10:56:18 +0530

    login feature

--- obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/DemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1df6956c7cbb3566205b0e2f1dd7eb30a180482")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
